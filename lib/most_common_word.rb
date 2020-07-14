3b47970ede15b82ed31337c8568b4f3b18d2a21f
Chris M <chris@adadevelopersacademy.org>
2020-07-14 16:50:57 -0700

    Update most_common_word.rb

--- lib/most_common_word.rb
+++ lib/most_common_word.rb
@@ -2,5 +2,9 @@
 # @param {String[]} banned
 # @return {String}
 def most_common_word(paragraph, banned)
+    if paragraph == ""
+        return nil
+    end
     
+    return paragraph.split(' ').first
 end
